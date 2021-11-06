7c993ea404a28ef1404cccd03fb0a992663329e3
WangJinzhong <andywang7259@163.com>
2021-11-07 07:45:35 +0800

    add md5, sha256, sha512 tsc

--- lib/utils/crypto-util.d.ts
+++ lib/utils/crypto-util.d.ts
@@ -1,4 +1,11 @@
+import { HexBase64Latin1Encoding } from 'crypto';
 declare const _default: {
+    /** 获取md5摘要 */
+    md5(value: string, encoding?: HexBase64Latin1Encoding): string;
+    /** 获取sha256摘要 */
+    sha256(value: string, encoding?: HexBase64Latin1Encoding): string;
+    /** 获取sha512摘要 */
+    sha512(value: string, encoding?: HexBase64Latin1Encoding): string;
     /** base64编码 */
     base64Encode(value: string): string;
     /** base64解码 */
@@ -18,4 +25,7 @@ declare const _default: {
      */
     aesDecrypt(data: string, key: string, iv: string, algorithm?: string): string;
 };
+/**
+ * http://nodejs.cn/api/crypto.html
+ */
 export default _default;
