d80bac5a0bf4b55832a4980875f5f5ac5a6b337d
WangJinzhong <andywang7259@163.com>
2023-04-21 16:32:56 +0800

    增加lint

---
+++ templates/stylelint/rules/recommended.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'unit-no-unknown': null,
+  'selector-type-no-unknown': null,
+  'font-family-no-missing-generic-family-keyword': null,
+  'no-descending-specificity': null,
+  'scss/at-import-partial-extension': null,
+  'function-url-quotes': null,
+  'selector-class-pattern': null,
+  'color-function-notation': 'legacy',
+  'selector-pseudo-element-no-unknown': null,
+  'alpha-value-notation': null,
+};
--- templates/stylelint/uni-app.project.js
+++ templates/stylelint/uni-app.project.js
@@ -1,16 +1,11 @@
 module.exports = {
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  extends: ['stylelint-config-standard'],
-  plugins: [
-    // https://github.com/kristerkari/stylelint-scss
-    'stylelint-scss',
+  extends: ['stylelint-config-standard', 'stylelint-config-standard-scss'],
+  plugins: ['stylelint-scss'],
+  overrides: [
+    {
+      files: ['**/*.{vue,html}'],
+      customSyntax: 'postcss-html',
+    },
   ],
-  rules: {
-    // https://stylelint.io/user-guide/rules/list
-    'unit-no-unknown': null,
-    'selector-type-no-unknown': null,
-    'font-family-no-missing-generic-family-keyword': null,
-    'no-descending-specificity': null,
-  },
+  rules: require('./rules/recommended'),
 };
