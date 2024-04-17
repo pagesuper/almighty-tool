43af5e3b0ad548790abb71077e95ccbd9af93847
WangJinzhong <wjinzhong@kinlong.cn>
2024-04-17 16:45:42 +0800

    20240417: upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 // https://eslint.org/docs/user-guide/configuring
 module.exports = {
   extends: [require.resolve('./templates/eslints/recommended.js')],
+  rules: {
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
+    'vue/no-unused-vars': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/no-template-key': 'off',
+    'vue/no-v-for-template-key': 'off',
+  }
 };
