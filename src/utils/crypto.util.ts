44bd9a2ecf306a0d8c200cffea5ef9236804613f
WangJinzhong <andywang7259@163.com>
2024-10-26 11:10:18 +0800

    20241026: 重构代码

--- src/utils/crypto.util.ts
+++ src/utils/crypto.util.ts
@@ -1,149 +1,120 @@
-import crypto from 'crypto';
-import deepmerge from 'deepmerge';
-
-interface IGenerateRsaKeyPairOptions {
-  modulusLength?: number;
-  publicExponent?: number;
-}
+import forge from 'node-forge';
 
 const cryptoUtil = {
-  // 私钥加密长文本
-  longPrivateEncrypt(privateKey: string | Buffer, buffer: string | Buffer) {
-    const aesKey = this.generateAesKey('aes-128-cbc');
-    const aesIv = this.generateAesIv('aes-128-cbc');
-    const encryptedAesKey = this.privateEncrypt(privateKey, aesKey);
-    const encryptedData = `${aesIv}${this.aesEncrypt(buffer.toString(), aesKey, aesIv)}`;
-    return { encryptedAesKey, encryptedData };
+  /** 获取md5摘要 */
+  md5(value: string): string {
+    const md5 = forge.md.md5.create();
+    md5.update(value);
+    return md5.digest().toHex();
   },
 
-  // 公钥解密长文本
-  longPublicDecrypt(publicKey: string | Buffer, encryptedAesKey: string, encryptedData: string) {
-    const key = this.publicDecrypt(publicKey, encryptedAesKey).toString();
-    const iv = encryptedData.slice(0, 16);
-    return this.aesDecrypt(encryptedData.slice(16), key, iv);
+  /** 获取sha256摘要 */
+  sha256(value: string): string {
+    const sha256 = forge.md.sha256.create();
+    sha256.update(value);
+    return sha256.digest().toHex();
   },
 
-  // 公钥加密长文本
-  longPublicEncrypt(publicKey: string | Buffer, buffer: string | Buffer) {
-    const aesKey = this.generateAesKey('aes-128-cbc');
-    const aesIv = this.generateAesIv('aes-128-cbc');
-    const encryptedAesKey = this.publicEncrypt(publicKey, aesKey);
-    const encryptedData = `${aesIv}${this.aesEncrypt(buffer.toString(), aesKey, aesIv)}`;
-    return { encryptedAesKey, encryptedData };
+  /** 获取sha512摘要 */
+  sha512(value: string): string {
+    const sha512 = forge.md.sha512.create();
+    sha512.update(value);
+    return sha512.digest().toHex();
   },
 
-  // 私钥解密长文本
-  longPrivatgeDecrypt(privateKey: string | Buffer, encryptedAesKey: string, encryptedData: string) {
-    const key = this.privateDecrypt(privateKey, encryptedAesKey).toString();
-    const iv = encryptedData.slice(0, 16);
-    return this.aesDecrypt(encryptedData.slice(16), key, iv);
-  },
-
-  // 私钥加密
-  privateEncrypt(privateKey: string | Buffer, buffer: string | Buffer) {
-    const key = typeof privateKey === 'string' ? Buffer.from(privateKey) : privateKey;
-    return crypto.privateEncrypt(key, Buffer.from(buffer)).toString('base64');
-  },
-
-  // 公钥解密
-  publicDecrypt(publicKey: string | Buffer, buffer: string | Buffer) {
-    const key = typeof publicKey === 'string' ? Buffer.from(publicKey) : publicKey;
-    return crypto.publicDecrypt(key, Buffer.from(buffer.toString(), 'base64')).toString();
-  },
-
-  // 公钥加密
-  publicEncrypt(publicKey: string | Buffer, buffer: string | Buffer) {
-    const key = typeof publicKey === 'string' ? Buffer.from(publicKey) : publicKey;
-    return crypto.publicEncrypt(key, Buffer.from(buffer)).toString('base64');
-  },
-
-  // 私钥解密
-  privateDecrypt(privateKey: string | Buffer, buffer: string | Buffer) {
-    const key = typeof privateKey === 'string' ? Buffer.from(privateKey) : privateKey;
-    return crypto.privateDecrypt(key, Buffer.from(buffer.toString(), 'base64')).toString();
-  },
-
-  // 生成 RSA 密钥对
-  generateRsaKeyPair(options: IGenerateRsaKeyPairOptions = {}) {
-    const defaultOptions = {
-      modulusLength: 4096,
-      publicKeyEncoding: {
-        type: 'spki',
-        format: 'pem',
-      },
-      privateKeyEncoding: {
-        type: 'pkcs8',
-        format: 'pem',
-      },
-    };
-    const mergedOptions = deepmerge(defaultOptions, options);
-    const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', mergedOptions);
-    return { publicKey: `${publicKey}`, privateKey: `${privateKey}` };
-  },
-
-  // 获取 MD5 摘要
-  md5(value: string, encoding: crypto.BinaryToTextEncoding = 'hex'): string {
-    return crypto.createHash('md5').update(value).digest(encoding);
-  },
-
-  // 获取 SHA256 摘要
-  sha256(value: string, encoding: crypto.BinaryToTextEncoding = 'hex'): string {
-    return crypto.createHash('sha256').update(value).digest(encoding);
-  },
-
-  // 获取 SHA512 摘要
-  sha512(value: string, encoding: crypto.BinaryToTextEncoding = 'hex'): string {
-    return crypto.createHash('sha512').update(value).digest(encoding);
-  },
-
-  // Base64 编码
+  /** base64编码 */
   base64Encode(value: string): string {
-    return Buffer.from(value).toString('base64');
+    return forge.util.encode64(value);
   },
 
-  // Base64 解码
+  /** base64解码 */
   base64Decode(value: string): string {
-    return Buffer.from(value, 'base64').toString();
+    return forge.util.decode64(value);
   },
 
-  // 生成随机 AES IV
-  generateAesIv(algorithm = 'aes-128-cbc'): string {
-    const keyLength = Number(algorithm.split('-')[1]) / 8;
-    return cryptoUtil.generateRandomString(keyLength);
+  generateAesKeyAndIV(algorithm: forge.cipher.Algorithm = 'AES-CBC'): { key: string; iv: string } {
+    let key: forge.Bytes;
+    let iv: forge.Bytes | null;
+
+    if (
+      algorithm === 'AES-ECB' ||
+      algorithm === 'AES-CBC' ||
+      algorithm === 'AES-CFB' ||
+      algorithm === 'AES-OFB' ||
+      algorithm === 'AES-CTR' ||
+      algorithm === 'AES-GCM'
+    ) {
+      // 对于AES相关算法，通常密钥长度可以是128位、192位或256位等，这里以128位（16字节）为例
+      key = cryptoUtil.generateRandomString(16);
+      // 对于需要初始向量的模式（如CBC、CFB、OFB、GCM等），AES的IV通常也是16字节
+      if (algorithm !== 'AES-ECB') {
+        iv = cryptoUtil.generateRandomString(16);
+      } else {
+        iv = null;
+      }
+    } else if (algorithm === '3DES-ECB' || algorithm === '3DES-CBC') {
+      // 3DES的密钥长度通常基于其自身特性，这里以标准的192位（24字节）为例
+      key = cryptoUtil.generateRandomString(24);
+      // 对于需要初始向量的模式（如3DES-CBC），3DES的IV通常是8字节
+      if (algorithm === '3DES-CBC') {
+        iv = cryptoUtil.generateRandomString(8);
+      } else {
+        iv = null;
+      }
+    } else if (algorithm === 'DES-ECB' || algorithm === 'DES-CBC') {
+      // DES的密钥长度通常是64位（8字节）
+      key = cryptoUtil.generateRandomString(8);
+      // 对于需要初始向量的模式（如DES-CBC），DES的IV通常是8字节
+      if (algorithm === 'DES-CBC') {
+        iv = cryptoUtil.generateRandomString(8);
+      } else {
+        iv = null;
+      }
+    } else {
+      throw new Error('unsupport the algorithm：' + algorithm);
+    }
+
+    return {
+      key,
+      iv: iv ?? '',
+    };
   },
 
-  // 生成随机 AES 密钥
-  generateAesKey(algorithm = 'aes-128-cbc'): string {
-    const keyLength = Number(algorithm.split('-')[1]) / 8;
-    return cryptoUtil.generateRandomString(keyLength);
-  },
-
-  // 生成随机字符串
+  /** 生成随机的字符串 */
   generateRandomString(length: number): string {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
+    let randomString = '';
+
     for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * characters.length));
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomString += characters[randomIndex];
     }
-    return result;
+
+    return randomString;
   },
 
-  // AES 加密
-  aesEncrypt(data: string, key: string, iv: string, algorithm = 'aes-128-cbc'): string {
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
-    cipher.setAutoPadding(true);
-    let encryptedData = cipher.update(data, 'utf8', 'base64');
-    encryptedData += cipher.final('base64');
-    return encryptedData;
+  /**
+   * 加密
+   *
+   * iv-length: 192/8
+   */
+  aesEncrypt(data: string, key: string, iv: string, algorithm: forge.cipher.Algorithm = 'AES-CBC'): string {
+    const cipher = forge.cipher.createCipher(algorithm, key);
+    cipher.start({ iv });
+    cipher.update(forge.util.createBuffer(data));
+    cipher.finish();
+    return forge.util.encode64(cipher.output.data);
   },
 
-  // AES 解密
-  aesDecrypt(data: string, key: string, iv: string, algorithm = 'aes-128-cbc'): string {
-    const decipher = crypto.createDecipheriv(algorithm, key, iv);
-    decipher.setAutoPadding(true);
-    let decryptedData = decipher.update(data, 'base64', 'utf8');
-    decryptedData += decipher.final('utf8');
-    return decryptedData;
+  /**
+   * 解密
+   */
+  aesDecrypt(data: string, key: string, iv: string, algorithm: forge.cipher.Algorithm = 'AES-CBC'): string {
+    const decipher = forge.cipher.createDecipher(algorithm, key);
+    decipher.start({ iv });
+    decipher.update(forge.util.createBuffer(forge.util.decode64(data)));
+    decipher.finish();
+    return decipher.output.data;
   },
 };
 
