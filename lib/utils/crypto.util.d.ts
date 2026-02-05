0e9ac6b073c3527fa6be6e3e161a3b28e166d264
WangJinzhong <andywang7259@163.com>
2026-02-05 11:17:52 +0800

    update: 20260205

--- lib/utils/crypto.util.d.ts
+++ lib/utils/crypto.util.d.ts
@@ -1,6 +1,5 @@
-import forge from 'node-forge';
 type KeyEncryptionScheme = 'RSAES-PKCS1-V1_5' | 'RSA-OAEP' | 'RAW' | 'NONE' | null;
-type CipherAlgorithm = forge.cipher.Algorithm;
+type CipherAlgorithm = 'AES-CBC' | 'AES-CTR' | 'AES-GCM' | 'AES-ECB';
 interface AesEncryptDecryptOptions {
     algorithm?: CipherAlgorithm;
 }
@@ -12,59 +11,35 @@ interface LongEncryptDecryptOptions extends EncryptDecryptOptions {
     joinSeparator?: string;
 }
 declare const cryptoUtil: {
-    /** 获取 uuid */
     uuid(): string;
-    /** 获取md5摘要 */
     md5(value: string): string;
-    /** 获取hmac摘要 */
-    hmac(key: string | forge.util.ByteBuffer | null, bytes: string, md?: forge.md.Algorithm): string;
-    /** 获取sha1摘要 */
+    hmac(key: string | null, bytes: string, md?: "sha1" | "sha256" | "sha512"): string;
     sha1(value: string): string;
-    /** 获取sha256摘要 */
     sha256(value: string): string;
-    /** 获取sha384摘要 */
     sha384(value: string): string;
-    /** 获取sha512摘要 */
     sha512(value: string): string;
-    /** base64编码 */
     base64Encode(value: string): string;
-    /** base64解码 */
     base64Decode(value: string): string;
-    /** 生成密钥对 */
     generateAesKeyAndIV(): {
         key: string;
         iv: string;
     };
-    /** 生成密钥Key */
     generateAesKey(length?: number): string;
-    /** 生成密钥IV */
     generateAesIv(length?: number): string;
-    /** 生成随机的字符串 */
     generateRandomString(length: number): string;
-    /**
-     * 加密
-     *
-     * iv-length: 192/8
-     */
     aesEncrypt(data: string, key: string, iv: string, options?: AesEncryptDecryptOptions): string;
-    /**
-     * 解密
-     */
     aesDecrypt(data: string, key: string, iv: string, options?: AesEncryptDecryptOptions): string;
-    /** ========= RSA 实现部分 ========= */
     generateRsaKeyPair(bits?: number): {
         publicKey: string;
         privateKey: string;
     };
     publicEncrypt(publicKey: string, data: string, options?: EncryptDecryptOptions): string;
     privateDecrypt(privateKey: string, encrypted: string, options?: EncryptDecryptOptions): string;
-    /** 长文本混合加密 */
     longPublicEncrypt(publicKey: string, data: string, options?: LongEncryptDecryptOptions): {
         encryptedAesKey: string;
         encryptedData: string;
     };
     longPrivateDecrypt(privateKey: string, encryptedAesKey: string, encryptedData: string, options?: LongEncryptDecryptOptions): string;
-    /** 辅助方法 */
     joinStrings(txt1: string, txt2: string, separator: string): string;
     splitJoinedStrings(str: string, separator: string): string[];
 };
